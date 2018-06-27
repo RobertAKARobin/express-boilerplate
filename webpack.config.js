8906180f2279298ca1cbb9004d71972f4ec27d46
Robin Thomas <>
2018-06-27 14:13:36 -0500

    Added Nodemon

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const NodemonPlugin = require('nodemon-webpack-plugin')
 
 module.exports = {
 	entry: './src/index.js',
@@ -8,6 +9,14 @@ module.exports = {
 		path: path.resolve(__dirname, 'public')
 	},
 	plugins: [
+		new NodemonPlugin({
+			watch: path.resolve("."),
+			script: path.resolve("./index.js"),
+			ignore: [
+				path.resolve("./public"),
+				path.resolve("./src")
+			]
+		}),
 		new MiniCssExtractPlugin({
 			filename: 'build.css'
 		})
